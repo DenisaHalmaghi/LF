65580c082fd41b772f68191e9cbf0d4da4d3f84f
DenisaHalmaghi <denisa.halmaghi93@gmail.com>
2020-10-13 13:45:44 +0300

    Add read from file and validation

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace First_App
 {
     class Program
     {
         static Random rand = new Random();
+        static List<string> neterminale = new List<string>();
+        static List<string> terminale = new List<string>();
+        static Dictionary<string, string[]> productii = new Dictionary<string, string[]>();
+
         static void Main(string[] args)
         {
 
-            string[] neterminale = { "E", "T", "F" };
-            string[] terminale = { "a", "+", "*", "," };
-            Dictionary<string, string[]> productii = new Dictionary<string, string[]>();
 
+            setup();
             //Map all productions to keys
-            productii.Add("E", new string[] { "E+T", "T" });
-            productii.Add("T", new string[] { "T*F", "F" });
-            productii.Add("F", new string[] { "a", "E" });
 
             //Starting string
-            string S = "E";
+            /*string S = "E";
 
             StringBuilder output = new StringBuilder(S); //Start with the starting string
 
@@ -45,7 +46,7 @@ namespace First_App
             }
 
             Console.WriteLine(output);
-            Console.WriteLine(output.Length);
+            Console.WriteLine(output.Length);*/
 
             //keep iterating over each symbol
 
@@ -58,5 +59,68 @@ namespace First_App
         {
             return array[rand.Next(0, length)];
         }
+
+        static void setup()
+        {
+            StreamReader sr = new StreamReader(System.IO.Path.GetFullPath(@"..\..\..\") + "setup.txt");
+            string contents = sr.ReadToEnd();
+           
+            MatchCollection matches = getParts(contents,"T");
+            foreach (Match match in matches)
+            {
+                terminale.Add(match.ToString());
+            }
+
+            matches = getParts(contents, "N");
+            foreach (Match match in matches)
+            {
+                neterminale.Add(match.ToString());
+            }
+
+            matches = getParts(contents, "P");
+            foreach (Match match in matches)
+            {
+                mapToProductions(match.ToString());
+            }
+           
+        }
+
+        static MatchCollection getParts(string contents,string part)
+        {
+            string value = Regex.Match(contents, part + @"=\{(?<word>.+)}").Groups["word"].Value;
+
+            return Regex.Matches(value, @"(?:(?<=\()[^()\s]+(?=\)))|(?:(?<!\()[^()\s,]+(?!\)))");
+        }
+
+        static void mapToProductions(string production)
+        {
+            string[] parts=Regex.Split(production, "->");
+            string key = parts[0];
+            string[] values=Regex.Split(parts[1], @"[|]");
+            //daca nu e neterminal nu avem ce mapa -> date gresite
+            if (!neterminale.Contains(key))
+            {
+                Console.WriteLine($"{key} nu se afla in multimea de neterminale! Va rog sa corectati");
+                System.Environment.Exit(1);
+            }
+
+            //verificam daca toate caracterele sunt fie terminale fie neterminale
+            //merge neterminale+terminale 
+            //^[{merged}]+$
+            string merged = $"^[{string.Join("", terminale.ToArray())}{string.Join("", neterminale.ToArray())}]+$";
+
+            foreach (string value in values)
+            {
+                if (!Regex.IsMatch(value, merged))
+                {
+                    Console.WriteLine($"{value} nu contine doar terminale sau neterminale! Va rog sa verificati lista de productii si sa corectati");
+                    System.Environment.Exit(1);
+                }
+            }
+
+            productii.Add(key,values);
+            
+        }
+
     }
 }
